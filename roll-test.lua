0c420f3100766a7bc0d8feaf8cf65938c4c2eef9
vshallc <vshallc@gmail.com>
2016-08-31 17:14:35 +0800

    add a demo

---
+++ roll-test.lua
@@ -0,0 +1,6 @@
+package.path=package.path .. ";/mnt/sdcard/touchelf/scripts/?.lua"     -- add default path to package path (For TouchElf)
+require "tb-utils"
+
+roll(2, 3, 4, 1, 6, 3, 4, 5, 2, 3, 4, 1, 6, 3, 4, 5, 2, 3, 4, 1, 6, 3, 4, 5, 2, 3, 4, 1, 6, 3, 4, 5, 2, 3)
+
+
